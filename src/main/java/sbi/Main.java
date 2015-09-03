34c1cd799dd689e3b6a870111685af46a87649a3
Sascha <sbi0991@gmail.com>
2015-09-03 22:44:13 +0200

    Added some more gradle examples Updated gradle wrapper to v2.6

--- src/main/java/sbi/Main.java
+++ src/main/java/sbi/Main.java
@@ -6,6 +6,9 @@ import sirius.kernel.Sirius;
 import sirius.kernel.di.std.Register;
 import sirius.kernel.health.Log;
 
+/**
+ * Main controler for this example project.
+ */
 @Register
 public class Main implements Lifecycle {
 
@@ -13,6 +16,13 @@ public class Main implements Lifecycle {
 
     @Override
     public void started() {
+        doStuff();
+    }
+
+    /**
+     * Does some stuff to test submodule and dependency usage.
+     */
+    private void doStuff() {
         LOG.WARN("started");
 
         Entity entity = new Entity(1234, "Gradle", "Pretty cool build tool", "BuildTool");
